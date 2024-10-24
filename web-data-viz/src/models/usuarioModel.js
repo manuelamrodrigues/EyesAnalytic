a89b66e88eecbc57fff2742128119d159774e429
kevellyoliveira <kevelly.oliveira@sptech.school>
2024-10-23 23:11:00 -0300

    add dependecias sweetalert

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -3,8 +3,7 @@ var database = require("../database/config")
 function autenticar( email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-
-    select idUsuario, nome, email, senha, fkEmpresa, fkTipoUsuario from usuario where email = '${email}' AND senha = '${senha}';
+    select idUsuario, nome, email, senha, situacao, fkEmpresa, fkTipoUsuario from usuario where email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
