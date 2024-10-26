9450d0d6318e29a1c85756ab1ef8f91f32a133f4
livianegrini-sptech <livia.negrini@sptech.school>
2024-10-26 00:37:28 -0300

    validacoes feitas no login e cadastro

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -16,7 +16,7 @@ function cadastrar(nome, email, senha, fkEmpresa, fkTipoUsuario) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fkEmpresa, fkTipoUsuario) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}', '${fkTipoUsuario}');
+        INSERT INTO usuario (nome, email, senha, fkEmpresa, fkTipoUsuario) VALUES ('${nome}', '${email}', '${senha}',  '${fkEmpresa}', '${fkTipoUsuario}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
