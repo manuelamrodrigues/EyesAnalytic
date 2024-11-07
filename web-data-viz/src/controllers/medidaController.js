82752331a6afb232ecd6aa6d508c83a7274d4587
JoaoOtavioF <joao.foliveira@sptech.school>
2024-11-07 16:56:56 -0300

    Estruturando para os gráficos

--- web-data-viz/src/controllers/medidaController.js
+++ web-data-viz/src/controllers/medidaController.js
@@ -2,13 +2,7 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
-
-    var idAquario = req.params.idAquario;
-
-    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
-
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas().then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -20,7 +14,6 @@ function buscarUltimasMedidas(req, res) {
         res.status(500).json(erro.sqlMessage);
     });
 }
-
 
 function buscarMedidasEmTempoReal(req, res) {
 
